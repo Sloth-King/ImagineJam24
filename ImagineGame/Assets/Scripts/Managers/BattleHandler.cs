2062248fab258498734a311a3ce4047bee5c13ae
Sloth-King <andrew.mansour24@gmail.com>
2024-11-23 17:10:32 +0100

    feat : basic combat (incomplete)

--- ImagineGame/Assets/Scripts/Managers/BattleHandler.cs
+++ ImagineGame/Assets/Scripts/Managers/BattleHandler.cs
@@ -1,22 +1,86 @@
 using UnityEngine;
 
-public class NewMonoBehaviourScript : MonoBehaviour
+public class BattleHandler : MonoBehaviour
 {
     [SerializeField] private GameObject _player;
 
-    private void Start()
-    {
-        SpawnCharacter(true);
-        SpawnCharacter(false);
+    public Sprite playerSprite;
+    public Sprite enemySprite;
+
+    private CharacterBattler playerCharacter;
+    private CharacterBattler enemyCharacter;
+    private CharacterBattler currentCharacter;
+    private BattleState battleState;
+
+    private static BattleHandler instance;
+
+    public static BattleHandler GetInstance(){
+        return instance;
     }
 
-    private void SpawnCharacter(bool isPlayerTeam){
+
+
+    private enum BattleState{
+        WaitingForInput,
+        Busy,
+    }
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    private void Start()
+    {
+        playerCharacter = SpawnCharacter(true);
+        enemyCharacter = SpawnCharacter(false);
+        SetActiveCharacter(playerCharacter);
+        battleState = BattleState.WaitingForInput;
+    }
+
+    private void Update()
+    {
+        if(battleState == BattleState.WaitingForInput){
+            if(Input.GetKeyDown(KeyCode.Space)){
+                battleState = BattleState.Busy;
+                playerCharacter.Attack(enemyCharacter, () => {
+                    //battleState = BattleState.WaitingForInput;
+                    ChooseNextCharacter();
+                });
+            }
+        }
+    }
+
+    private CharacterBattler SpawnCharacter(bool isPlayerTeam){
         Vector3 spawnPosition = new Vector3(0, 0, 0);
         if(isPlayerTeam){
-            spawnPosition = new Vector3(-45, 0, 0);
+            spawnPosition = new Vector3(-4.2f, 0, 0);
         }else{
-            spawnPosition = new Vector3(45, 0, 0);
+            spawnPosition = new Vector3(4.2f, 0, 0);
         }
-        Instantiate(_player, spawnPosition, Quaternion.identity);
+        GameObject character = Instantiate(_player, spawnPosition, Quaternion.identity);
+        CharacterBattler characterBattler = character.GetComponent<CharacterBattler>();
+        characterBattler.Setup(isPlayerTeam);
+
+        return characterBattler;
+    }
+
+    private void SetActiveCharacter(CharacterBattler character){
+        if(currentCharacter != null){
+            currentCharacter.HideSelectionCircle();
+        }
+        currentCharacter = character;
+        currentCharacter.ShowSelectionCircle();
+    }
+
+    private void ChooseNextCharacter(){
+        if(currentCharacter == playerCharacter){
+            SetActiveCharacter(enemyCharacter);
+            currentCharacter.Attack(playerCharacter, () => {
+                    ChooseNextCharacter();
+            });
+        }else{
+            SetActiveCharacter(playerCharacter);
+            battleState = BattleState.WaitingForInput;
+        }
     }
 }
