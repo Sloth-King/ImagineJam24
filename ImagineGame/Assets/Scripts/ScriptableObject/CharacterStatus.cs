94a590874f8598d70f0652f1b3f756899f68a1c6
Sloth-King <andrew.mansour24@gmail.com>
2024-11-23 11:21:57 +0100

    clean : project structure

---
+++ ImagineGame/Assets/Scripts/ScriptableObject/CharacterStatus.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class CharacterStatus : ScriptableObject
+{
+    public string characterName;
+    public int currentHealth;
+    public int maxHealth = 100;
+    public GameObject characterPrefab;
+
+    //if we have time : 
+
+    //public int currentMana;
+    //public int maxMana = 100;
+    //public int level;
+    //public int experience;
+    //public int experienceToNextLevel;
+
+    
+}
